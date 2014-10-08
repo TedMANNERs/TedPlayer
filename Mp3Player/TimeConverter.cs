86f5cc18dcbb0620b37b886c7c891425e48d5cdb
TedMANNERs <lukas-braegger@hotmail.com>
2014-10-08 13:53:26 +0200

    Improved label format for passed time

---
+++ Mp3Player/TimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Mp3Player
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    public class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TimeSpan time = TimeSpan.FromSeconds((int)value);
+            return string.Format("{0}:{1:D2}", time.Minutes, time.Seconds);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
